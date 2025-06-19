cb237eb2863f9a82a3aedb1c94cdccb6d67a2c93
복준수 <brianbok97@gmail.com>
2025-06-19 17:58:24 +0900

    Add: producer message dto to alim
    alim 서비스 발행 dto 작성

---
+++ src/main/java/com/gorae/gorae_post/kafka/producer/alim/dto/CommentRegisterEvent.java
@@ -0,0 +1,25 @@
+package com.gorae.gorae_post.kafka.producer.alim.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class CommentRegisterEvent {
+    public static final String TOPIC = "comment-register";
+    
+    // 수정인지 새로 작성한것인지
+    // register or modify
+    private String action;
+
+    private String userId;
+    private String postId;
+    private String postTitle;
+    private String comment;
+
+    private LocalDateTime eventTime;
+
+}
