0d3b08c699b89a156d9077a031b3c0a713111000
복준수 <brianbok97@gmail.com>
2025-06-21 21:48:39 +0900

    Add: 게시판 CRUD service, controller
    질문 생성, 수정, 삭제 모두 POST 메서드로 통일
    질문 미리보기, 상세보기, 페이지네이션 기능 추가

--- src/main/java/com/gorae/gorae_post/service/QuestionService.java
+++ src/main/java/com/gorae/gorae_post/service/QuestionService.java
@@ -1,26 +1,29 @@
 package com.gorae.gorae_post.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.gorae.gorae_post.common.dto.ApiResponseDto;
 import com.gorae.gorae_post.domain.dto.comment.Comment;
-import com.gorae.gorae_post.domain.dto.question.Question;
-import com.gorae.gorae_post.domain.dto.question.QuestionActionRequest;
-import com.gorae.gorae_post.domain.dto.question.QuestionForm;
+import com.gorae.gorae_post.domain.dto.question.*;
 import com.gorae.gorae_post.domain.dto.user.UserInfo;
+import com.gorae.gorae_post.domain.dto.user.UserInfoDto;
 import com.gorae.gorae_post.domain.repository.QuestionRepository;
 import com.gorae.gorae_post.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.nio.file.AccessDeniedException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -30,43 +33,182 @@ public class QuestionService {
     private final UserRepository userRepository;
 
     @Transactional
-    public void create(QuestionActionRequest actionRequest, String userId) {
-        QuestionForm questionForm = actionRequest.getPayload();
-        Question question = questionForm.toEntity(userId);
+    public Map<String,Object> mapContent(String contentJson) throws JsonProcessingException {
+        // 저장된 String block 을 역직렬화 하는 함수
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(contentJson, new TypeReference<>() {});
+    }
+
+    @Transactional
+    public Long create(QuestionCreateDto questionCreateDto, String userId) {
+        Question question = questionCreateDto.toEntity(userId);
+        questionRepository.save(question);
+        return question.getId();
+    }
+
+    @Transactional
+    public void update(QuestionUpdateDto questionUpdateDto, String userId) throws ChangeSetPersister.NotFoundException, AccessDeniedException, JsonProcessingException {
+        Long questionId = questionUpdateDto.getQuestionId();
+        ObjectMapper mapper = new ObjectMapper();
+        Question question = questionRepository.findById(questionId)
+                .orElseThrow(() -> new ChangeSetPersister.NotFoundException());
+        if (!question.getUserId().equals(userId)) {
+            throw new AccessDeniedException("본인 글만 수정이 가능합니다.");
+        }
+
+        question.setTitle(questionUpdateDto.getTitle());
+        question.setContentJson(mapper.writeValueAsString(questionUpdateDto.getContent()));
+        question.setUpdateAt(LocalDateTime.now());
         questionRepository.save(question);
     }
 
     @Transactional
-    public void update(QuestionActionRequest actionRequest, String userId) throws ChangeSetPersister.NotFoundException, AccessDeniedException {
-        Long questionId = actionRequest.getQuestionId();
+    public void delete(Long questionId, String userId) throws ChangeSetPersister.NotFoundException, AccessDeniedException {
         Question question = questionRepository.findById(questionId)
                 .orElseThrow(() -> new ChangeSetPersister.NotFoundException());
-        if (question.getUserId().equals(userId)) {
-            throw new AccessDeniedException("본인 글만 수정이 가능합니다.");
+        if (!question.getUserId().equals(userId)) {
+            throw new AccessDeniedException("본인 글만 삭제가 가능합니다.");
         }
-        question.setQuestionTitle(question.getQuestionTitle());
-        question.setQuestionContent(question.getQuestionContent());
-        question.setUpdateAt(LocalDateTime.now());
+        if (!question.getCommentList().isEmpty()) {
+            throw new AccessDeniedException("답변이 달린 글은 삭제가 불가능합니다.");
+        }
+        question.setDisplay(false);
+        questionRepository.save(question);
+    }
+
+    private QuestionOverviewDto convertOverviewDto(Question question, String keyword) throws ChangeSetPersister.NotFoundException, JsonProcessingException {
+        String userId = question.getUserId();
+        UserInfo userInfo = userRepository.findById(userId)
+                .orElseThrow(() -> new ChangeSetPersister.NotFoundException());
+
+        UserInfoDto userInfoDto =UserInfoDto.builder()
+                    .userId(userInfo.getUserId())
+                    .userName(userInfo.getUserName())
+                    .profileImgUrl(userInfo.getProfileImgUrl())
+                    .userBadge(userInfo.getUserBadge())
+                    .build();
+
+        // Question Entity class 인스턴스를 질문 미리보기 타입으로 변경해주는 함수
+        return QuestionOverviewDto.builder()
+                .keyword(keyword == null ? "":keyword)
+                .questionId(question.getId())
+                .userInfoDto(userInfoDto)
+                .title(question.getTitle())
+                .previewContent(mapContent(question.getContentJson()))
+                .writer(question.getUserId())
+                .build();
+    }
+
+    private List<CommentDto> convertToCommentDtoList(Question question, UserInfoDto userInfoDto) {
+        List<Comment> commentList = question.getCommentList();
+        List<CommentDto> commentDtoList = new ArrayList<>();
+        for (Comment comment : commentList) {
+            CommentDto dto = CommentDto.builder()
+                    .commentId(comment.getId())
+                    .commentContent(comment.getCommentContent())
+                    .updateAt(comment.getUpdateAt())
+                    .userInfoDto(userInfoDto)
+                    .build();
+            commentDtoList.add(dto);
+        }
+        return commentDtoList;
     }
 
     @Transactional
-    public void delete() {
+    public QuestionListDto overview(int page, int size, String keyword, String sort, String order) {
+        Pageable pageable = PageRequest.of(page-1,size, Sort.by(Sort.Direction.DESC, sort));
 
+        Page<Question> questionPage = questionRepository.findByDisplayTrue(pageable);
+
+        List<QuestionOverviewDto> overviewDtos = questionPage.getContent()
+                .stream()
+                .map(question -> {
+                    try {
+                        return convertOverviewDto(question,keyword);
+                    } catch (ChangeSetPersister.NotFoundException e) {
+                        throw new RuntimeException(e);
+                    } catch (JsonProcessingException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .collect(Collectors.toList());
+
+        QuestionListDto dto = new QuestionListDto(
+                overviewDtos,
+                page,
+                size,
+                questionPage.getTotalPages(),
+                questionPage.getTotalElements()
+        );
+        return dto;
     }
 
+    @Transactional
+    public QuestionListDto search(int page, int size, String keyword, String sort, String order) {
+        Pageable pageable = PageRequest.of(page-1,size, Sort.by(Sort.Direction.DESC,sort));
+
+        Page<Question> questionPage = questionRepository.findByDisplayTrueAndTitleContainingOrContentJsonContaining(keyword,keyword,pageable);
+
+        List<QuestionOverviewDto> overviewDtos = questionPage.getContent()
+                .stream()
+                .map(question -> {
+                    try {
+                        return convertOverviewDto(question,keyword);
+                    } catch (ChangeSetPersister.NotFoundException e) {
+                        throw new RuntimeException(e);
+                    } catch (JsonProcessingException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .toList();
+
+        QuestionListDto dto = new QuestionListDto(
+                overviewDtos,
+                page,
+                size,
+                questionPage.getTotalPages(),
+                questionPage.getTotalElements()
+        );
+        return dto;
+    }
 
     @Transactional
-    public String detail(Long questionId) throws ChangeSetPersister.NotFoundException, JsonProcessingException {
+    public QuestionDetailDto detail(Long questionId) throws ChangeSetPersister.NotFoundException, JsonProcessingException {
+        // 질문 조회
         Question question = questionRepository.findById(questionId)
                 .orElseThrow(() -> new ChangeSetPersister.NotFoundException());
 
-        String userId = question.getUserId();
-        Optional<UserInfo> userInfo = userRepository.findById(userId);
-        Map<String, Object> map = Map.of(
-                "question", question,
-                "userInfo", userInfo
-        );
+        // 삭제된 질문 조회시
+        if (!question.getDisplay()) {
+            throw new ChangeSetPersister.NotFoundException();
+        }
 
-        return new ObjectMapper().writeValueAsString(map);
+        // viewCount 증가
+        Integer viewCount = question.getViewCount();
+        question.setViewCount(viewCount + 1);
+        questionRepository.save(question);
+
+        // 유저 정보 조회
+        UserInfo userInfo = userRepository.findById(question.getUserId())
+                .orElseThrow(() -> new ChangeSetPersister.NotFoundException());
+
+        UserInfoDto userInfoDto =UserInfoDto.builder()
+                .userId(userInfo.getUserId())
+                .userName(userInfo.getUserName())
+                .profileImgUrl(userInfo.getProfileImgUrl())
+                .userBadge(userInfo.getUserBadge())
+                .build();
+
+        List<CommentDto> commentDtoList = convertToCommentDtoList(question,userInfoDto);
+
+        return QuestionDetailDto.builder()
+                .questionId(question.getId())
+                .title(question.getTitle())
+                .detailContent(mapContent(question.getContentJson()))
+                .userInfo(userInfoDto)
+                .updateAt(question.getUpdateAt())
+                .viewCount(question.getViewCount())
+                .commentList(commentDtoList)
+                .build();
     }
 }
