6f5ad94e6542e9cb2093af0c7bf1bde3cb2d3503
jangseungwon08 <jangseungwon9819@gmail.com>
2025-06-22 15:30:52 +0900

    좋아요 기능 구현

--- src/main/java/com/gorae/gorae_post/api/open/LikeController.java
+++ src/main/java/com/gorae/gorae_post/api/open/LikeController.java
@@ -1,24 +1,28 @@
-package com.gorae.gorae_interaction.api.open;
+package com.gorae.gorae_post.api.open;
 
 
-import com.gorae.gorae_interaction.common.dto.ApiResponseDto;
-import com.gorae.gorae_interaction.common.exception.ApiError;
-import com.sun.security.auth.UserPrincipal;
+import com.gorae.gorae_post.common.dto.ApiResponseDto;
+import com.gorae.gorae_post.domain.dto.like.Like;
+import com.gorae.gorae_post.domain.dto.like.LikeDto;
+import com.gorae.gorae_post.service.LikeService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import java.security.Principal;
 
 @Slf4j
 @RestController
-@RequestMapping(value = "/api/post/v1/", produces = MediaType.APPLICATION_JSON_VALUE)
-public class InteractionController {
+@RequiredArgsConstructor
+@RequestMapping(value = "/api/post/v1", produces = MediaType.APPLICATION_JSON_VALUE)
+public class LikeController {
+    private final LikeService likeService;
 
-    @PostMapping(value = "/{commentId}/like")
-    public ApiResponseDto<String> commentLike(@PathVariable Long commentId, String userId){
+    @PostMapping(value = "/like")
+    public ApiResponseDto<String> commentLike(@RequestBody @Valid LikeDto likeDto){
 //        TODO: @AuthenticationPrincipal UserPrincipal userPrincipal로 유저 아이디 구현
-        return ApiResponseDto.defaultOk();
+        likeService.like(likeDto);
+        return ApiResponseDto.createOk("좋아요 요청 성공");
     }
 }
--- src/main/java/com/gorae/gorae_post/domain/dto/like/Like.java
+++ src/main/java/com/gorae/gorae_post/domain/dto/like/Like.java
@@ -3,12 +3,14 @@ package com.gorae.gorae_post.domain.dto.like;
 import com.gorae.gorae_post.domain.dto.comment.Comment;
 import com.gorae.gorae_post.domain.dto.user.UserInfo;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@Builder
 public class Like {
 
     @Id
