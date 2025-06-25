04dd314e4be1aa8d2574bc8501d8e1e365941a18
jangseungwon08 <jangseungwon9819@gmail.com>
2025-06-25 20:30:18 +0900

    Update CommentEvent.java

--- src/main/java/com/gorae/gorae_post/kafka/producer/leaderboard/dto/CommentEvent.java
+++ src/main/java/com/gorae/gorae_post/kafka/producer/leaderboard/dto/CommentEvent.java
@@ -9,12 +9,11 @@ import lombok.Setter;
 @Setter
 public class CommentEvent {
     public static final  String TOPIC = "comment-produce";
-    private String commentId;
+    private String commentUserId;
 
     public static CommentEvent fromEntity(Comment comment){
         CommentEvent event = new CommentEvent();
-        Long id = comment.getId();
-        event.setCommentId(String.valueOf(id));
+        event.setCommentUserId(comment.getUserInfo().getUserId());
         return event;
     }
 
